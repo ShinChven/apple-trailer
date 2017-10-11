0b90ec185053185a43eaf4664454f2d0edc2baea
ShinChven Zhang <shinchven@gmail.com>
2017-10-11 22:17:14 +0800

    move files to src

--- test/test-buildurl.js
+++ test/test-buildurl.js
@@ -1,4 +1,4 @@
-const parser = require('../lib/apple-trailer-url-parser');
-const quality = require('../lib/quality');
+const parser = require('../src/apple-trailer-url-parser');
+const quality = require('../src/quality');
 var urls = parser.buildUrl('lucasfilm', 'star-wars-the-last-jedi', 'star-wars-the-last-jedi-trailer-1', quality.h1080p_mov);
 console.log(urls);
--- test/test-find-all-trailers.js
+++ test/test-find-all-trailers.js
@@ -1,5 +1,5 @@
-const parser = require('../lib/apple-trailer-url-parser');
-const quality = require('../lib/quality');
+const parser = require('../src/apple-trailer-url-parser');
+const quality = require('../src/quality');
 var urls = parser.findAllClipsInAllQualities('independent', 'the-stray', 3);
 
 urls.forEach(function (t) {
