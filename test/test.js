bc5a750b4c6ee113a8361cd19a2df82662b0e035
ShinChven <shinchven@gmail.com>
2018-02-21 20:38:41 +0800

    update README.md

--- test/test.js
+++ test/test.js
@@ -4,6 +4,7 @@ const appleTrailer = require('../src/index');
  * node test/test.js http://trailers.apple.com/trailers/magnolia/please-stand-by/
  */
 function main() {
+    // set trailer page url and video quality.
     let url = process.argv[2] || 'http://trailers.apple.com/trailers/magnolia/please-stand-by/';
     let quality =  process.argv[3];
     console.log('home:');
@@ -11,13 +12,14 @@ function main() {
     console.log('');
 
     appleTrailer
-        .getPageData(url)
+        .getPageData(url) // get page data
         .then(function (data) {
             let title = data.page.movie_title;
             console.log('title:');
             console.log(title);
             console.log('');
 
+            // get video url.
             let videoUrls = appleTrailer.getVideoUrl(data, quality);
             console.log('video urls:');
             videoUrls.forEach(function (videoUrl) {
